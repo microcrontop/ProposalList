5807d352674d62e1a19b215c6ee10efb149649b3
microcrontop <microcrontop@gmail.com>
2021-02-09 16:00:09 +0200

    add data

--- index.js
+++ index.js
@@ -5,11 +5,13 @@ const ETH = {
     url: 'https://mainnet.infura.io/v3/45174a29359d4b07ade01676259bc47a',
     fromBlock: '11739872',
     address: '0x278cDd6847ef830c23cac61C17Eab837fEa1C29A',
+    erc20HandlerAddress: '0xB8B493600A5b200Ca2c58fFA9dced00694fB3E38',
 };
 const AVA = {
     url: "https://api.avax.network/ext/bc/C/rpc",
     fromBlock: '34246',
     address: "0xee8aE1088D02CCDA2CDd0FdA2381DB679d0b122E",
+    erc20HandlerAddress: "0x40a07f36655A0724557cA53A9E5D1b5018e9Df32",
 };
 
 const config = ETH;
@@ -27,7 +29,8 @@ bridgeContract.getPastEvents("ProposalEvent", {fromBlock: config.fromBlock}, (er
                 "depositNonce", e.returnValues.depositNonce,
                 "status", e.returnValues.status,
                 "resourceID", e.returnValues.resourceID,
-                "dataHash", e.returnValues.dataHash
+                "dataHash", e.returnValues.dataHash,
+                'data', web3.utils.keccak256(config.erc20HandlerAddress + data.slice(2))
             );
         }
     };
